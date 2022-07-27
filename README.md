d906eaf2b86a562d089acebac5b70fb3f961aea1
Durafen <duracula@gmail.com>
2022-07-27 03:11:36 +0300

    google translate, sums, 5 group by

--- README.md
+++ README.md
@@ -1,36 +1,30 @@
-##  바이낸스 코인선물 트레이딩 호가창(Binance API Future Trading Program)
+## Binance API Future Trading Program 
 
-바이낸스 선물 거래와 증권사 파생상품 거래를 경험해 하면서 거래량의 큰 차이가 안나는 데, 바이낸스 선물의 체결속도는 상대적으로 느리다고 인지됐다. 더 좋은 체결속도를 위해 API를 사용한 매매프로그램을 만들어야 겠다고 인지했다.
+Having experienced Binance futures trading and securities company derivatives trading, it was recognized that there was not a large difference in the volume of transactions, and that the execution rate of Binance futures was relatively slow. I realized that I needed to create a trading program using an API to speed up execution. 
 
-### 1. Purpose
-- 바이낸스 선물 UI에 답답함을 느껴 한국의 HTS 파생선물 호가창과 동일한 UI로 구현
-- 자동매매를 원하지만 주요 메서드를 모르는 투자자들을 위해 제공
-- coin-m, usd-m 바이낸스 객체생성시 변경가능
+### 1. Purpose 
+- I feel frustrated with the Binance Futures UI and implement it in the same UI as the HTS Derived Futures Hogachang in Korea 
+- Offered for investors who want to make automatic trades but do not know the main methods 
+- coin-m, usd-m can be changed when creating Binance objects
 
-
-### 2. UI Interface
-----
+ ### 2. UI Interface
+  ---- 
 ![KakaoTalk_20220209_235029338](https://user-images.githubusercontent.com/40832965/153558802-e102a735-e89e-4f79-bb4d-29ef26cee503.png)
-----
+  ---- 
+  
+  ### 3. Demo VIdeo 
+  - Black window under UI is Binance window 
 
-### 3. Demo VIdeo
- - UI밑에 검은 창은 바이낸스 창
-
-https://user-images.githubusercontent.com/40832965/157466507-d5f65489-a3d6-42a5-9f29-00caeca9eb17.mp4
-
-
-### 4. Overview
-- 코인선물 설정 바에서 코인이름 선택하면 해당 코인 호가창출력
-- 매수 버튼클릭 시 코인 1개 LONG 포지션
-- 매도 버튼 클릭 시 코인 1개 SHORT 포지션
-- 주문일괄취소 누르면 모든 주문 취소
-- 자동매매를 만들고 싶다면 위 메서드를 활용하면 가능
-
-
-### 5. Requirements
-- binance 사이트에서 Future API 등록("api.txt" 파일 생성 후 저장)
-- Python3, PYQT , QTdesigner
-
-
-
-
+  https://user-images.githubusercontent.com/40832965/157466507-d5f65489-a3d6-42a5-9f29-00caeca9eb17.mp4
+  
+  
+  ### 4. Overview 
+  - If you select the coin name in the coin gift setting bar, the corresponding coin bid window will be printed 
+  - 1 LONG position of coins when you click the buy button 
+  - 1 SHORT position of coins when you click the sell button 
+  - Cancel Batch Orders Cancel All Orders 
+  - If you want to create an automatic trade, you can do so by using the above method. 
+  
+  ### 5. Requirements 
+  - Register the Future API on the binance site (create and save the "api.txt" file) 
+  - Python3, PYQT, QTdesigner
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+from syslog import setlogmask
 import ccxt
 import time
 import datetime
@@ -10,7 +11,7 @@ from PyQt5.QtCore import Qt, QThread, pyqtSignal
 import timeit
 
 # API키를 저장한 텍스트 파일 불러오기
-with open("api.txt") as f:
+with open("src/api.txt") as f:
     lines = f.readlines()
     api_key = lines[0].strip()
     secret = lines[1].strip()
@@ -21,46 +22,49 @@ binance = ccxt.binance(config={
     'secret': secret,
     'enableRateLimit': True,
     'options': {
-        'defaultType': 'delivery'
+        'defaultType': 'future'
     }
 })
+
+# binance.set_position_mode(hedged=False)
 
 #전역변수
 global order
 global order_num_list
 global position
 order_num_list = []
-g_tick_data = 'ADA/USD' #현재 호가창 및 거래를 할 코인종류 전역변수
+g_tick_data = 'APE/USDT' #현재 호가창 및 거래를 할 코인종류 전역변수
 
 # 1 바이낸스 API 통신 세팅
 markets = binance.load_markets()
-binance.verbose = True
 
 '''API메서드'''
 class BinanceFunction():
     #현재가 조회
     def present_price(self):
         ticker = binance.fetch_ticker(g_tick_data)
-        print(ticker['open'], ticker['high'], ticker['low'], ticker['close'])
-        print('현재가 : ',ticker['close'])
+        # print(ticker['open'], ticker['high'], ticker['low'], ticker['close'])
+        # print('current price: ',ticker['close'])
         return ticker['close']
 
     #매수
     def buy_long(self):
         global position
         position = self.present_price()
-        order = binance.create_order(g_tick_data, "limit", "buy", 1, price=position, params={})  #(코인 종류, 시장가or지정가, 포지션, 수량, 가격)
+        quantity = 6 / position
+        order = binance.create_order(g_tick_data, "limit", "buy", quantity, price=position, params={})  #(코인 종류, 시장가or지정가, 포지션, 수량, 가격)
         order_num_list.append(order)
-        print('매수전송', order)
+        print('buy', order)
        
 
     #매도
     def sell_short(self):
         global position
         position = self.present_price()
-        order = binance.create_order(g_tick_data, "limit", "sell", 1, price=position, params={})  #(코인 종류, 시장가or지정가, 포지션, 수량, 가격)
+        quantity = 6 / position
+        order = binance.create_order(g_tick_data, "limit", "sell", quantity, price=position, params={})  #(코인 종류, 시장가or지정가, 포지션, 수량, 가격)
         order_num_list.append(order)
-        print('매도전송', order)
+        print('sell', order)
 
     #주문 취소
     def cancel_lifo_order(self):
@@ -68,14 +72,14 @@ class BinanceFunction():
         order_id = temp['info']['orderId']
         order_cancel = binance.cancel_order(order_id, g_tick_data)
         order_num_list.pop()
-        print('주문취소완료',order_cancel)
+        print('Order Cancellation Completed',order_cancel)
 
     def cancel_all_order(self):
         for i in range(len(order_num_list)):
             temp = order_num_list[i]
             order_id = temp['info']['orderId']
             order_cancel = binance.cancel_order(order_id, g_tick_data)
-            print('주문 일괄취소 완료',order_cancel)
+            print('Completed bulk order cancellation',order_cancel)
         order_num_list.clear()
 
     #잔고확인
@@ -96,11 +100,11 @@ class OrderbookWorker(QThread):
 
     def run(self):
         while self.alive:
-            print('통신코인',g_tick_data)
+            # print('symbol,',g_tick_data)
             start_time = timeit.default_timer()
-            data = binance.fetch_order_book(g_tick_data, limit=10)  #limit 변경시 2가지 변경, 1.pyqt사이즈 2.호가창 리셋for문
+            data = binance.fetch_order_book(g_tick_data, limit=50,)  #limit 변경시 2가지 변경, 1.pyqt사이즈 2.호가창 리셋for문
             terminate_time = timeit.default_timer()
-            print("%f초 걸렸습니다." % (terminate_time - start_time))
+            print("%f It took seconds." % (terminate_time - start_time))
 
             time.sleep(0.00001)
             self.dataSent.emit(data)
@@ -129,7 +133,7 @@ class OrderbookWorker(QThread):
 class OrderbookWidget(QWidget,BinanceFunction):
     def __init__(self, parent=None, ticker="TRX/USD"):
         super().__init__(parent)
-        uic.loadUi("mainUI.ui", self)
+        uic.loadUi("src/mainUI.ui", self)
         self.ticker = ticker
         #item
         #코인 종류 고르는 매서드
@@ -165,14 +169,11 @@ class OrderbookWidget(QWidget,BinanceFunction):
                                      "\n"
                                      "}")
 
-         '''매수, 매도 버튼 이벤트'''
         self.buy_present.setStyleSheet("background-color : #58FA82;") ##00FF00  "color: #FAFAFA;"
         self.buy_present.clicked.connect(self.buy_long)
 
         self.sell_present.setStyleSheet("background-color : #ff5522;")
         self.sell_present.clicked.connect(self.sell_short)
-
-        '''취소 버튼 이벤트'''
 
         self.cancel_all.setStyleSheet("background-color : #ff7f50  ;")
         self.cancel_all.clicked.connect(self.cancel_all_order)
@@ -180,13 +181,17 @@ class OrderbookWidget(QWidget,BinanceFunction):
     # ----------------- 호가창 데이터 공간 세팅 ------------------
         for i in range(10):
             #  매도호가 : 2열데이터
+            item_0 = QTableWidgetItem(str(""))
+            item_0.setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+            self.tableAsks.setItem(i, 0, item_0)
+
             item_2 = QTableWidgetItem(str(""))
             item_2.setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
             self.tableAsks.setItem(i, 2, item_2)
 
             # 매도 물량 : 1열데이터
             item_1 = QTableWidgetItem(str(""))
-            item_1.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            item_1.setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
             self.tableAsks.setItem(i, 1, item_1)
 
 
@@ -196,8 +201,12 @@ class OrderbookWidget(QWidget,BinanceFunction):
             self.tableBids.setItem(i, 2, item_2)
             # 매수물량 : 3열데이터
             item_3 = QTableWidgetItem(str(""))
-            item_3.setTextAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+            item_3.setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
             self.tableBids.setItem(i, 3, item_3)
+
+            item_4 = QTableWidgetItem(str(""))
+            item_4.setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+            self.tableBids.setItem(i, 4, item_4)
 
 
         self.ow = OrderbookWorker(self.ticker)
@@ -211,17 +220,49 @@ class OrderbookWidget(QWidget,BinanceFunction):
         global g_tick_data #전역변수로 설정하여 유동적으로 다른코인 호가창으로 변경가능
         g_tick_data = str(self.coin_box.currentText())
 
-        for i, v in enumerate(data['asks'][::-1]): #매도 i= 0 매도 v= [2.13441, 311.0]
-            item_2 = self.tableAsks.item(i, 2)
-            item_2.setText(f"{v[0]:,}")
-            item_1 = self.tableAsks.item(i, 1)
-            item_1.setText(f"{v[1]:,}")
+        asks = []        
+        sum = 0
+        prices = 0
+        for i in range(1, len(data['asks']) +1):
+            prices += data['asks'][i-1][0]
+            sum += data['asks'][i-1][1]
+            
+            if (i % 5 == 0):
+                asks.append([round(prices / 5, 4), sum])
+                prices, sum = 0,0
 
-        for i, v in enumerate(data['bids']):
+        bids = []        
+        sum = 0
+        prices = 0
+        for i in range(1, len(data['bids']) +1):
+            prices += data['bids'][i-1][0]
+            sum += data['bids'][i-1][1]
+            
+            if (i % 5 == 0):
+                bids.append([round(prices / 5, 4), sum])
+                prices, sum = 0,0
+                
+        asks_sum = 0        
+        for i, v in enumerate(asks): #매도 i= 0 매도 v= [2.13441, 311.0]
+            item_2 = self.tableAsks.item(9-i, 2)
+            item_2.setText(f"{v[0]:,}")
+            item_1 = self.tableAsks.item(9-i, 1)
+            item_1.setText(f"{round(v[1]):,}")
+
+            asks_sum += v[1]
+            item_0 = self.tableAsks.item(9-i, 0)
+            item_0.setText(f"{round(asks_sum):,}")
+
+        bids_sum = 0
+        for i, v in enumerate(bids):
             item_2 = self.tableBids.item(i, 2)
             item_2.setText(f"{v[0]:,}")
             item_3 = self.tableBids.item(i, 3)
-            item_3.setText(f"{v[1]:,}")
+            item_3.setText(f"{round(v[1]):,}")
+            
+            bids_sum += v[1]
+            item_4 = self.tableBids.item(i, 4)
+            item_4.setText(f"{round(bids_sum):,}")
 
 
     def updataBalance(self, balance, ticker):
